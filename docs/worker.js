b0a3a2fbfcea599c7d1b7739af6a3af427f5eef5
ghinorhino <218945672+ghinorhino@users.noreply.github.com>
2025-12-20 18:00:29 +0100

    add sw and manifest for PWA

---
+++ docs/worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/index.html',
+        '/index.css',
+        '/json.css',
+        '/json.html',
+        '/xml.css',
+        '/xml.html',
+        '/favicon.png',
+        '/favicon-192.png',
+        '/favicon-512.png',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
